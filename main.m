268cfef2373219e71d74153611cdc8c14bae4017
Peter Teeson <pteeson@icloud.com>
2016-06-07 15:40:53 -0400

    Initial commit by PHT

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Moriarty
+//
+//  Created by Peter Teeson on 11-02-10 at 2:53 PM.
+//  Copyright 2011. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
